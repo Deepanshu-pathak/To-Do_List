4a97201b0deb00c47b6c89b7eb63d0107254fc48
Deepanshu <119618517+Deepanshu-pathak@users.noreply.github.com>
2023-10-29 19:10:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 let inputbx = document.querySelector('#inputbx');
 let list = document.querySelector('#list');
 
+// Load items from localStorage if available
+const savedItems = JSON.parse(localStorage.getItem('items')) || [];
+
+// Function to save the items to localStorage
+function saveToLocalStorage() {
+    localStorage.setItem('items', JSON.stringify(savedItems));
+}
+
+// Function to load items from localStorage and add them to the list
+function loadFromLocalStorage() {
+    for (const item of savedItems) {
+        addItem(item);
+    }
+}
+
 // toggle button for dark mode
 const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
-toggle.onclick = function(){
+toggle.onclick = function () {
     toggle.classList.toggle('switch');
     body.classList.toggle('switch');
-}
+};
 
-inputbx.addEventListener("keyup", function(event){
-    if (event.key == "Enter"){
-        addItem(this.value);
+// Event listener for Enter key
+inputbx.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        const newItem = this.value;
+        addItem(newItem);
+        savedItems.push(newItem);
+        saveToLocalStorage();
         this.value = "";
     }
-})
+});
 
-let addItem = (inputbx) => {
+// Function to add an item to the list
+function addItem(inputbx) {
     let listItem = document.createElement("li");
     listItem.innerHTML = `${inputbx}<p></p>`;
-    listItem.addEventListener("click", function(){
+    listItem.addEventListener("click", function () {
         this.classList.toggle('done');
-    })
-    listItem.querySelector("p").addEventListener("click", function(){
+    });
+    listItem.querySelector("p").addEventListener("click", function () {
         listItem.remove();
-    })
+        const itemIndex = savedItems.indexOf(inputbx);
+        if (itemIndex > -1) {
+            savedItems.splice(itemIndex, 1);
+            saveToLocalStorage();
+        }
+    });
     list.prepend(listItem);
 }
+
+// Load items from localStorage when the page loads
+loadFromLocalStorage();
+
